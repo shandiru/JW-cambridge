1b9eda9237e4b73ca2673949c2488bb27b9c5f71
STharshan <sivatharshan81@gmail.com>
2026-01-25 00:35:12 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import TermsConditions from "./components/Term";
 import PrivacyPolicy from "./components/PrivacyPolicy";
 import GDPRConsent from "./components/GDPRButton";
 import ScrollToTop from "./components/ScrollToTop";
-import Home from "./page/Home"
-import Service from "./page/Service"
+import Home from "./page/Home";
+import Service from "./page/Service";
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: true,     // animate once
+      offset: 100,
+    });
+  }, []);
+
   return (
     <Router>
       <ScrollToTop />
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/terms" element={<TermsConditions />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/service" element={<Service />} />
       </Routes>
+
       <Footer />
       <GDPRConsent />
     </Router>
